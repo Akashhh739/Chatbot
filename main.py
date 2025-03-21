8101efad93088dc15f9390e7dca1e59e535217bc
Akashhh739 <148942680+Akashhh739@users.noreply.github.com>
2025-03-21 13:34:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 # Your Groq API key (hardcoded)
 API_URL = "https://gateway.ai.cloudflare.com/v1/b8687b6abfce956eb0b143563cd63721/b-o-t/groq/openai/v1/chat/completions"
 
+
 # Cloudflare Gateway endpoint for Chat completions
 API_URL = https://gateway.ai.cloudflare.com/v1/b8687b6abfce956eb0b143563cd63721/b-o-t/groq/openai/v1/chat/completions
 
