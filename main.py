53241656141f6a7e3eea4341744883d73e8c68a6
Akashhh739 <148942680+Akashhh739@users.noreply.github.com>
2025-03-21 13:32:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import requests
 API_KEY = "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"
 
 # Cloudflare Gateway endpoint for Chat completions
-API_URL = "https://gateway.ai.cloudflare.com/v1/b8687b6abfce956eb0b143563cd63721/b-o-t/groq/chat/completions"
+API_URL = https://gateway.ai.cloudflare.com/v1/b8687b6abfce956eb0b143563cd63721/b-o-t/groq/openai/v1/chat/completions
+
 
 def generate_response(prompt, model, temperature, max_tokens):
     headers = {
